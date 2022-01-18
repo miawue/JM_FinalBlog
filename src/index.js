6036deeafaf2f34452f2ac61e13b58aeb5dee5fc
Alexseesmyname <skalpel550@gmail.com>
2022-01-18 23:37:23 +0300

    Added img, updated index.js/css, created Post

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
+import './index.css';
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
